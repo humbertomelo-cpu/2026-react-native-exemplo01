840f702084756a2011449030b734a9a3414af703
betomelo_9 <betomelo_9@hotmail.com>
2026-02-23 19:57:52 -0300

    exemplo 01

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('');
+ 
+  const[idade, setIdade] = useState(0);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{ marginTop: 20 }} >
+        <TextInput 
+         style={styles.input}
+         placeholder="Digite algo aqui"
+         onChangeText={(text) => setText(text)}
+         />
+      </View>
+      <View style={{ marginTop: 20 }} >
+        <Button onPress={() => alert(text)} title="Clique Aqui" />
+      </View>
+      <View style={{ marginTop: 20 }} >
+        <Text>Você digitou: {text}</Text>
+      </View>
+      <View style={{ marginTop: 20 }}>
+      <TextInput style={styles.input}
+       placeholder='Digite sua Idade'
+       onChangeText={(valor) => setIdade(Number(valor))}
+      />
+      </View>
+      <View style={{ marginTop: 20 }}>
+       <Text>Sua idade é: {idade}</Text>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +45,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%',
+  }
 });
